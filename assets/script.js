3048e4aaf505325b71953e9c6ff616339e045ad4
j-winston <kenjameswinston@gmail.com>
2022-10-11 15:08:45 -0400

    partial display implementation

--- assets/script.js
+++ assets/script.js
@@ -70,9 +70,14 @@ function clearScreen(){
 function storeValue() {
     const userInput = this.textContent;
     expression.store(userInput);
-    
-    
+
 }
+
+function displayNumber() {
+    const userInput = this.textContent;
+    expression.display(userInput);
+}
+
 
 function setOperationMode() {
     const mode = this.textContent;
@@ -103,6 +108,7 @@ function endExpression() {
 let expression = {
     a:0,
     b:0,
+    temp: 0,
     operationMode: '',
     result: 0,
     inMidCalculation: false,
@@ -111,6 +117,13 @@ let expression = {
         if(expression.inMidCalculation){
             expression.b += userInput
             expression.b = parseInt(expression.b);
+            expression.inMidCalculation = false;
+
+            // expression.temp = expression.a;
+            // expression.a = "";
+            // expression.clearDisplay();
+            // expression.a = expression.temp;
+            // expression.temp = 0;
 
 
             
@@ -128,9 +141,12 @@ let expression = {
             expression.result = answer;
             expression.a = expression.result;
             expression.b = 0;
-        }
+        } 
         expression.inMidCalculation = true;
         expression.operationMode = operator;
+
+        
+        
 
 
     },
@@ -146,7 +162,21 @@ let expression = {
         expression.operationMode = '',
         expression.result = 0,
         expression.inMidCalculation = false;
+        expression.clearDisplay();
+    },
+
+
+    display(inputValue) {
+        document.querySelector('.screen-text').textContent += inputValue; 
+    },
+
+
+    clearDisplay(){
+        document.querySelector('.screen-text').textContent = "";
+
     }
+
+    
    
     
 }
@@ -155,6 +185,7 @@ let expression = {
 // Keypad event listeners
 // Get User Input
 document.querySelectorAll('.num-btn').forEach((btn) => btn.addEventListener('click', storeValue));
+document.querySelectorAll('.num-btn').forEach((btn) => btn.addEventListener('click', displayNumber));
 
 document.querySelector('.add-btn').addEventListener('click', setOperationMode);
 document.querySelector('.multiply-btn').addEventListener('click', setOperationMode);
