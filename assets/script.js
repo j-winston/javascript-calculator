55ab6864b03c405ae99617b35e801f6ae29ad749
j-winston <kenjameswinston@gmail.com>
2022-10-13 07:08:30 -0400

    display properly outputs all expressions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 
-
 function add(a, b) {
     return (a + b);
 }
@@ -38,6 +37,7 @@ function operate(a, operator, b) {
 
 
 function allClear() {
+    // TODO--Change to expression.clearMemory()
     expression.clear();
    
 }
@@ -47,20 +47,20 @@ function storeOperator(){
     expression.operator = this.textContent;
 }
 
-
+ // TODO--Remove this, see line 76
 function displayNumber(value) {
     document.querySelector('.screen-text').textContent = "";
     document.querySelector('.screen-text').textContent += value;
 
 }
 
-
+// TODO-- Delete this 
 function printInput() {
     document.querySelector('.screen-text').textContent += this.textContent;
 
 }
 
-
+// TODO-- Delete this
 function clearScreen(){
     document.querySelector('.screen-text').textContent = "";
 
@@ -82,19 +82,19 @@ function displayNumber() {
 function setOperationMode() {
     const mode = this.textContent;
     expression.setMode(mode);
-    // remove console.log
-    
-    
+  
 }
 
 
 function equals() {
-    // remove console.log
-    console.log('Equals:',expression.getAnswer());
+    expression.clearDisplay();
+    expression.display(expression.getAnswer());
+    // TODO--output to screen here
     
 }
 
 
+// TODO--Remove both of these, not used anywhere
 function startNewExpression() {
     expression.nowCalculating = true;
 }
@@ -104,6 +104,7 @@ function endExpression() {
     expression.nowCalculating = false;
 }
 
+// Expression's functions
 
 let expression = {
     a:0,
@@ -112,42 +113,41 @@ let expression = {
     operationMode: '',
     result: 0,
     inMidCalculation: false,
+    newEntry: true,
 
     store(userInput) {
+        // TODO--Change to function call inMidCalculation()
         if(expression.inMidCalculation){
             expression.b += userInput
             expression.b = parseInt(expression.b);
             
-
-            // expression.temp = expression.a;
-            // expression.a = "";
-            // expression.clearDisplay();
-            // expression.a = expression.temp;
-            // expression.temp = 0;
-
-
-            
+       
         }else{
             expression.a += userInput;
             expression.a = parseInt(expression.a);
         }
         
+        
     },
 
     setMode(operator) {
         // If mid-calculation, increment result each time user presses +,-,x,/''
+        // TODO--Decouple this create a function that checks if in middle of calculation
         if(expression.inMidCalculation){
             const answer = expression.getAnswer();
             expression.result = answer;
             expression.a = expression.result;
+            // TODO--This might adversely impact history of expressions and 
+            // may not be necessary 
             expression.b = 0;
         } 
         expression.inMidCalculation = true;
         expression.operationMode = operator;
         console.log(expression.result);
 
-        
-        
+        // To clear screen for next entry
+        // Change to startNewEntry()
+        expression.newEntry = true;
 
 
     },
@@ -155,8 +155,10 @@ let expression = {
     getAnswer() {
         const answer = operate(expression.a, expression.operationMode, expression.b);
         return answer;
+        // TODO--Create a function that saves the last expression
     },
 
+    // TODO--Change name to clearMemory()
     clear() {
         expression.a = 0;
         expression.b = 0;
@@ -168,7 +170,16 @@ let expression = {
 
 
     display(inputValue) {
+         // TODO--Create a function isNewEntry() that checks if it should clear display
+        if(expression.newEntry){
+            expression.clearDisplay();
+            expression.newEntry = false;
+            
+        }
+        
         document.querySelector('.screen-text').textContent += inputValue; 
+        
+        
     },
 
 
@@ -176,11 +187,8 @@ let expression = {
         document.querySelector('.screen-text').textContent = "";
 
     }
-
     
-   
-    
-}
+} // End main object
 
 
 // Keypad event listeners
